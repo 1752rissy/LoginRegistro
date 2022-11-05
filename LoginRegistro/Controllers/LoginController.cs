4f29bc33b8b06c77417fbc4a48a3af3ecfe6e6d2
Andrés Gutierrez <andres.gutierrez@externos-cl.cencosud.com>
2022-11-05 18:11:04 -0300

    Agregar archivos de proyecto.

---
+++ LoginRegistro/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using LoginRegistro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginRegistro.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ColegioEntities _db = new ColegioEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult LogearUsuario()
+        {
+            return View("LogearUsuario");
+        }
+
+        [HttpPost]
+        public ActionResult LogearUsuario(Usuario _usuario)
+        {
+            var h_password = GetMd5(_usuario.Password);
+            var data = _db.Usuario.Where(u => u.Email.Equals(_usuario.Email) && u.Password.Equals(h_password)).ToList();
+            if(data.Count() > 0)
+            {
+                //añadir a la sesion del usuario
+                Session["NombreApellido"] = data.FirstOrDefault().Nombre + " " + data.FirstOrDefault().Apellido;
+                Session["Email"] = data.FirstOrDefault().Email;
+                Session["idUsuario"] = data.FirstOrDefault().idUsuario;
+                return RedirectToAction("index");
+            }
+            else
+            {
+                ViewBag.error = "Fallo el login";
+                return RedirectToAction("index");
+            }
+        }
+
+        public static string GetMd5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] TargeData = md5.ComputeHash(fromData);
+
+            string byte25 = null;
+
+            for (int i = 0; i < TargeData.Length; i++)
+            {
+                byte25 += TargeData[i].ToString("x2");
+            }
+            return byte25;
+        }
+    }
+}
---
+++ LoginRegistro/Controllers/RegistroController.cs
@@ -0,0 +1,64 @@
+﻿using LoginRegistro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginRegistro.Controllers
+{
+    public class RegistroController : Controller
+    {
+        private ColegioEntities _db = new ColegioEntities();
+        // GET: Registro
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Registrar()
+        {
+            return View("Registrar");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registrar(Usuario _user)
+        {
+            if(ModelState.IsValid)
+            {
+                var check = _db.Usuario.FirstOrDefault(u => u.Email == _user.Email);
+                if(check == null)
+                {
+                    _user.Password = GetMd5(_user.Password);
+                    _db.Configuration.ValidateOnSaveEnabled = false;
+                    _db.Usuario.Add(_user);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return View();
+        }
+
+
+        public static string GetMd5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] TargeData = md5.ComputeHash(fromData);
+
+            string byte25 = null;
+
+            for (int i = 0; i < TargeData.Length; i++)
+            {
+                byte25 += TargeData[i].ToString("x2");
+            }
+
+            return byte25;
+        }
+
+
+    }
+}
